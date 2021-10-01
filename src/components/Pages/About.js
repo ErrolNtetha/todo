141f2a9e8971e29d485a80bbd91a45482bda4d85
ErrolNtetha <mphumier@gmail.com>
2021-10-01 22:43:24 +0200

    added a new About page

---
+++ src/components/Pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <section>
+                Hello, this is about!
+            </section>
+        </React.Fragment>
+    )
+}
