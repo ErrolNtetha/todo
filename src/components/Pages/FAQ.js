19025fdd979d1e3e011da7bc14d4aa29e939edab
ErrolNtetha <mphumier@gmail.com>
2021-10-01 22:44:31 +0200

    added a new FAQ page

---
+++ src/components/Pages/FAQ.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function FAQ() {
+    return (
+        <React.Fragment>
+            <section>
+                Hello, this is FAQ!
+            </section>
+        </React.Fragment>
+    )
+}
