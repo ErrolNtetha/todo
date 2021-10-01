c88d98ab82711921677b25800864291386a58de6
ErrolNtetha <mphumier@gmail.com>
2021-10-01 22:46:09 +0200

    created a new directory

---
+++ src/components/Pages/Activity/Activity.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Activity() {
+    return (
+        <React.Fragment>
+            <section>
+                <section className='todoContainer'>
+                    <h3>These are to dos!!</h3>
+                </section>
+            </section>
+        </React.Fragment>
+    )
+}
