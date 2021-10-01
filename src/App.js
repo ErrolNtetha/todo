0e2b022f8fdc18829af5c41fde7d043e43f6784f
ErrolNtetha <mphumier@gmail.com>
2021-10-01 22:42:35 +0200

    imported routers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import Header from './components/Pages/Home/Header';
-import Intro from './components/Pages/Home/Intro'
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Pages/Home/Home'
+import Header from './components/Pages/Home/Header'
+import Activity from './components/Pages/Activity/Activity';
+import About from './components/Pages/About';
+import FAQ from './components/Pages/FAQ';
+
+
 import './App.css'
 import './styles/style.scss'
-import Features from './components/Pages/Home/Features';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Intro />
-      <Features />
-    </div>
+    <Router>
+        <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/activities" component={Activity} />
+        <Route path="/about" component={About} />
+        <Route path="/faq" component={FAQ} />
+      </Switch>
+    </Router> 
   );
 }
 
