cb6cefb385d1b0012a048a59f906cf6eafda622a
ErrolNtetha <mphumier@gmail.com>
2021-10-02 12:49:27 +0200

    added a new component

---
+++ src/components/Pages/CreateAccount.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function CreateAccount() {
+    return (
+        <React.Fragment>
+            <section className="createContainer">
+                <label htmlFor="Name"> Name </label>
+                <input type="text" name="name" placeholder="First Names" id="" />
+                <label htmlFor="Name"> Name </label>
+                <input type="text" name="name" placeholder="First Names" id="" />
+                <label htmlFor="Name"> Name </label>
+                <input type="text" name="name" placeholder="First Names" id="" />
+            </section>
+        </React.Fragment>
+    )
+}
