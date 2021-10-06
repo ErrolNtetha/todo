38306d2bd827c7778666ee27ee32378abf0566de
ErrolNtetha <mphumier@gmail.com>
2021-10-06 11:43:21 +0200

    fixed the padding and margin

--- src/components/Pages/Login.js
+++ src/components/Pages/Login.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Login() {
     return (
         <React.Fragment>
             <section className="loginContainer">
                 <section className="textFields">
-                    <label htmlFor="Name"> Name </label>
-                    <input type="text" name="name" placeholder="First Names" id="" />
+                    <span>
+                        <label htmlFor="Name"> Username or Email: </label>
+                        <input type="text" name="name" placeholder="Enter username or Email" id="" />
+                    </span>
+                    <span>
+                        <label htmlFor="Name"> Password: </label>
+                        <input type="password" name="name" placeholder="Password" id="" />
+                        <Link>
+                        Login
+                        </Link>
+                    </span>
+                    
+
                 </section>
             </section>           
         </React.Fragment>
