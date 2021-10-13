4f6ab1304864cf13708ea144ff37ab46a5659c9e
ErrolNtetha <mphumier@gmail.com>
2021-10-13 21:32:26 +0200

    created a user schema

--- src/backend/schema/user.schema.js
+++ src/backend/schema/user.schema.js
@@ -1,13 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema()
 
+// This is a Document === Schema
 const userSchema = new Schema({
-    firstName: String,
-    lastName: String,
+    name: {
+        firstName: String,
+        lastName: String,
+    },
     occupation: String,
     dateOfBirth: { type: Date, default: Date.now },
-})
+});
 
 const userModel = mongoose.model('User', userSchema);
+
+// Create a document
+const add = new userModel({
+    name: {
+        firstName: "Mphumeleli",
+        lastName: "Ntetha",
+    },
+    occupation: "Student",
+})
+
+// Check if data was saved and log the object, else catch an error
+add.save()
+    .then(response => console.log("Details saved successfully... ", response))
+    .catch(err => console.log("Error saving to the database: ", err))
 
 module.exports = userModel;
