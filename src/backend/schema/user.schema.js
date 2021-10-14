ca66820b7a0bfde7af152f7b683eb61999ee9b30
ErrolNtetha <mphumier@gmail.com>
2021-10-14 13:48:34 +0200

    restructured a schema to be basic

--- src/backend/schema/user.schema.js
+++ src/backend/schema/user.schema.js
@@ -1,30 +1,11 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema()
 
 // This is a Document === Schema
-const userSchema = new Schema({
-    name: {
-        firstName: String,
-        lastName: String,
-    },
-    occupation: String,
-    dateOfBirth: { type: Date, default: Date.now },
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
 });
 
-const userModel = mongoose.model('User', userSchema);
+const UserModel = mongoose.model('User', userSchema);
 
-// Create a document
-const add = new userModel({
-    name: {
-        firstName: "Mphumeleli",
-        lastName: "Ntetha",
-    },
-    occupation: "Student",
-})
-
-// Check if data was saved and log the object, else catch an error
-add.save()
-    .then(response => console.log("Details saved successfully... ", response))
-    .catch(err => console.log("Error saving to the database: ", err))
-
-module.exports = userModel;
+module.exports = UserModel;
