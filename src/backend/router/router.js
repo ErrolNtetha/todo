537e77f4540ff5c6e21a5ecd7e06ed6b866e92e6
ErrolNtetha <mphumier@gmail.com>
2021-10-17 15:12:06 +0200

    fixed my routes

--- src/backend/router/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const register_user = require('../controllers/userController');
-const home = require('../controllers/userController');
-
-router.get('/', home);
-router.post('/signup', register_user);
-
-module.exports = router
