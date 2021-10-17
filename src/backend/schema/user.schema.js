f4139a585892884f4aa69844553cba95c06e538b
ErrolNtetha <mphumier@gmail.com>
2021-10-17 15:13:22 +0200

    added few fields and validations that will be used later

--- src/backend/schema/user.schema.js
+++ src/backend/schema/user.schema.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 
-// This is a Document === Schema
+ 
+/*
+* This is a Schema - a.k.a Document
+* Define all possible fields for a user in the website
+*/
 const userSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    password: String,
+    name: { firstName: { type: String, required: true, }, firstName: { type: String, required: true, }, },
+    email: { type: String, required: [true, 'Please provide email address!'], },
+    password: { type: String, min: 8, required: true, },
+    description: String,
+    age: { type: Number, min: [16, 'Too young!'], },
+    gender: { type: String, enum: ['Male', 'Female'], },
 });
 
 const UserModel = mongoose.model('User', userSchema);
