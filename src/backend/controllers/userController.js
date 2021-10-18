cae1c674db12c4cbd48543d5f4e101a04a6b1e55
ErrolNtetha <mphumier@gmail.com>
2021-10-18 13:02:57 +0200

    folder for controllers

---
+++ src/backend/controllers/userController.js
@@ -0,0 +1,58 @@
+const User = require('../schema/user.schema'); // Import the user Schema
+const bcrypt = require('bcrypt');
+
+/*
+* Create controllers for routes and export them to userRouter file
+* As part of the MVC architecture, here in this project i try to follow that pattern
+  where i seperate my routes, controllers and views for readability and mantainability
+*/
+
+exports.userList = (req, res) => {
+    // Get all the users already in database and list them all in "/users"
+    res.send("Users list");
+}
+git 
+// Catch all user data from the form in front-end
+exports.register_user = (req, res) => {
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if(err) {
+            return res.status(500).json({
+                error: err
+            })
+        }
+        else {
+            const password = hash;
+            const firstName = req.body.firstName;
+            const lastName = req.body.lastName;
+            const email = req.body.email;
+
+    /*
+    * Get the data from the from the front-end
+    * Initialize all Schema field with the data
+    */
+    const data = new User({
+        name: {
+            firstName,
+            lastName,
+        },
+        email,
+        password,
+    });
+
+    /* 
+    * Save that in the Mongoose
+    * Search if the id exists in mock database using the id in the param
+    */
+    data.save(function(err, req, res) {
+        if(err) {
+            console.log("Error saving to the database...", err);
+        }
+        else {
+            console.log(data)
+        }
+    })
+        }
+    })
+
+    
+}
