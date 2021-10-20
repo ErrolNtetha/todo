b5257fad553e89a2c0205860b0382a6a12cb0863
ErrolNtetha <mphumier@gmail.com>
2021-10-20 21:40:14 +0200

    new icons to corresponding task

--- src/components/Pages/Activity/TaskList.js
+++ src/components/Pages/Activity/TaskList.js
@@ -1,22 +1,19 @@
 import React, { useContext } from 'react';
 import { TodoContext } from '../../../TodoContext';
-import { FaCheckCircle } from 'react-icons/fa';
+import { FaCheckCircle, FaTrash } from 'react-icons/fa';
 
 export default function TaskList() {
     const [todos, setTodos ] = useContext(TodoContext);
 
-    console.log(todos[0].tasks)
-    // Array(4) [ "cleaning", "studying", "laundry", "lunch with family" ]
-
     return (
         <section className="taskListContainer">
             <section className="taskSubContainer">
-                <h2> Due Today {todos.length} </h2>
+                <h2> Due Today <span>  </span></h2>
                 <section className="taskList">
-                   { todos[0].tasks.map((item, key) => {
+                   { todos[0].tasks?.map((item, key) => {
                        return (
                         <div className="task" key={item.id}>
-                            <p> {item} <span> <FaCheckCircle />  </span> </p>
+                            <p> {item} <span> <FaCheckCircle /> <FaTrash className="faTrash" /> </span> </p>
                             <hr />
                         </div>
                        )
