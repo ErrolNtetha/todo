4bad12a1e6b14bfec7f858d4d7682e9133874b7e
ErrolNtetha <mphumier@gmail.com>
2021-10-20 15:29:12 +0200

    added a map to list all todos in the dom

--- src/components/Pages/Activity/TaskList.js
+++ src/components/Pages/Activity/TaskList.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { TodoContext } from '../../../TodoContext';
+import { FaCheckCircle } from 'react-icons/fa';
 
 export default function TaskList() {
+    const [todos, setTodos ] = useContext(TodoContext);
+
+    console.log(todos[0].tasks)
+    // Array(4) [ "cleaning", "studying", "laundry", "lunch with family" ]
+
     return (
         <section className="taskListContainer">
             <section className="taskSubContainer">
-                <h2> Due Today </h2>
+                <h2> Due Today {todos.length} </h2>
                 <section className="taskList">
-                    <p> These are the tasks</p>
+                   { todos[0].tasks.map((item, key) => {
+                       return (
+                        <div className="task" key={item.id}>
+                            <p> {item} <span> <FaCheckCircle />  </span> </p>
+                            <hr />
+                        </div>
+                       )
+                   }) }
                 </section>
             </section>
         </section>
